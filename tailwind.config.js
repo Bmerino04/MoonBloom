1d1b91246b378b9cc5e40a1deed98ae95e5be0f6
Bmerino04 <bmerinomunoz0404@gmail.com>
2024-09-11 12:10:31 -0300

    fix tailwind.config.js content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.{html,js}","./dist/views/*.{html,js}", "./dist/Landing/*.{html,js}","./dist/Information/*.{html,js},/dist/Login-Register-view"],
+  content: ["./dist/*.{html,js}",
+    "./dist/views/*.{html,js}",
+    "./dist/views/Information/*{html,js}",
+    "./dist/views/Login-Register-view/*{html,js}",
+    "./dist/views/Main-View/*{html,js}",
+    "./dist/views/Statistic-view/*{html,js}"
+  ],
   theme: {
     extend: {},
   },
