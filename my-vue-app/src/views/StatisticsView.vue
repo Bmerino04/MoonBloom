0b7fd350baebb9d4ed5c7190b99c809f8b5514f0
ValentinaC <v.cifuentes04@ufromail.cl>
2024-10-20 16:33:04 -0300

    create phases slider component

--- my-vue-app/src/views/StatisticsView.vue
+++ my-vue-app/src/views/StatisticsView.vue
@@ -1,10 +1,11 @@
 <script setup>
  import Navbar from '../components/navbar.vue'
+ import Slider from '../components/PhasesSlider.vue'
 </script>
 <template>
     <div class="Statistics-page">
         <Navbar :currentView="'estadisticas'" />
-        
+        <Slider/>
     </div>
 </template>
 
