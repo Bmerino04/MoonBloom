6399ee604a0b6eaf945b53e9315ee02e44055856
ValentinaC <v.cifuentes04@ufromail.cl>
2024-10-20 16:10:28 -0300

    feat: navbar integration

---
+++ my-vue-app/src/views/StatisticsView.vue
@@ -0,0 +1,15 @@
+<script setup>
+ import Navbar from '../components/navbar.vue'
+</script>
+<template>
+    <div class="Statistics-page">
+        <Navbar :currentView="'estadisticas'" />
+        
+    </div>
+</template>
+
+<style scoped>
+.Statistics-page{
+    background-color: #A6C4C2;
+}
+</style>
