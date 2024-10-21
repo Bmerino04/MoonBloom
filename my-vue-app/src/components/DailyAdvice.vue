0198761d7e349987786f93316191499edae67dc9
vivivi <v.castro05@ufromail.cl>
2024-10-21 05:56:58 -0300

    create DailyAdvice.vue component first version

---
+++ my-vue-app/src/components/DailyAdvice.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="Daily-Advice">
+        <h3>CONSEJO DEL DIA</h3>
+        <p>
+            Mantente hidratada, come alimentos ricos en antioxidantes y escucha a tu cuerpo si sientes molestias.
+        </p>
+    </div>
+</template>
+<style scoped>
+.Daily-Advice{
+    background-color: var(--color-celeste-oscuro-op);
+    border-radius: 0.5rem;
+    padding: 2% 3% 3%;
+    text-align: justify;
+}
+
+.Daily-Advice h3{
+    color:  var(--color-rosado-claro-barras); 
+    letter-spacing: 0.1rem;  
+}
+.Daily-Advice p{
+    font-size:  var(--tamaño-fuente-texto);
+    color: var(--color-blanco);
+    padding: 2% 0;
+}
+</style>
