2e8c4a19c9bf26fcdb05381fb79cbcc721d91305
ValentinaC <v.cifuentes04@ufromail.cl>
2024-10-22 17:41:33 -0300

    feat(SexualTrendSlider): first funcional version

--- my-vue-app/src/components/SexualTrendSlider.vue
+++ my-vue-app/src/components/SexualTrendSlider.vue
@@ -1,67 +1,68 @@
 <template>
-    <div class="barChart">
-      <canvas :id="chartId"></canvas>
-    </div>
-  </template>
-  
-  <script setup>
-  import { onMounted, watch } from 'vue';
-  import Chart from 'chart.js/auto';
-  
-  const props = defineProps({
-    chartId: {
-      type: String,
-      required: true
-    },
-    labels: {
-      type: Array,
-      required: true
-    },
-    data: {
-      type: Array,
-      required: true
-    }
-  });
-  
-  let chartInstance = null;
-  
-  const createBarChart = () => {
-    const ctx = document.getElementById(props.chartId).getContext('2d');
-    chartInstance = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: props.labels,
-        datasets: [{
-          label: 'Tendencia Sexual',
-          data: props.data,
-          backgroundColor: ['#48A09F', '#FF7777']
-        }]
-      },
-      options: {
-        responsive: true
-      }
-    });
-  };
-  
-  onMounted(() => {
-    createBarChart();
-  });
-  
-  watch(() => props.data, (newData) => {
-    if (chartInstance) {
-      chartInstance.data.datasets[0].data = newData;
-      chartInstance.update();
-    }
-  });
-  </script>
-  
-  <style scoped>
-  .barChart {
-    background-color: var(--color-rosado-claro);
-    border:8px solid var(--color-rosado-claro-barras);
-    border-radius: 8px;
-    width: 100%;
-    height: 100%;
-    margin-top: 0px;
-  }
-  </style>
+  <div class="sexual-trend-slider">
+    <h2>{{ `Ciclo ${currentSlideIndex + 1}` }}</h2>
+      <div class="buttons">
+        <button @click="previousSlide" class="back-button">
+          <img src="../assets/img_cal/left-arrow.png" alt="Anterior" />
+        </button>
+        <button @click="nextSlide" class="next-button">
+          <img src="../assets/img_cal/right-arrow.png" alt="Siguiente" />
+        </button>
+      </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const cycles = ref([]);
+const currentSlideIndex = ref(0);
+
+const nextSlide = () => {
+  currentSlideIndex.value = (currentSlideIndex.value + 1) % cycles.value.length;
+};
+
+const previousSlide = () => {
+  currentSlideIndex.value = (currentSlideIndex.value - 1 + cycles.value.length) % cycles.value.length;
+};
+</script>
+
+<style scoped>
+.slide{
+  display: flex;
+  width: 54%;
+  height: auto;
+  justify-content: center;
+}
+.sexual-trend-slider {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-bottom: 0px;
+  padding-top: 2.6%;
+  margin-left: 7%;
+}
+button{
+    background-color:transparent;
+    border-color:transparent;
+}
+.buttons {
+  width: 66px;
+    height: auto;
+    display: flex;
+    position: absolute;
+    z-index: 2;
+    align-items: center;
+    justify-content: center;
+    margin-left: 47%;
+    background-color: var(--color-rosado-claro-barras);
+    border-bottom-left-radius: 10px;
+    border-top-right-radius: 10px;
+    text-align: center;
+    gap: 0px;
+}
+.buttons img{ 
+  width: 100%;
+  padding-top: 5px;
+}
+</style>
