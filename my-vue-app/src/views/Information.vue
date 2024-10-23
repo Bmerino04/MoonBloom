ea16c38d0be16a4d819c14f773b975ebed694b34
vivivi <v.castro05@ufromail.cl>
2024-10-23 03:13:39 -0300

    feat Information view data

--- my-vue-app/src/views/Information.vue
+++ my-vue-app/src/views/Information.vue
@@ -1,7 +1,10 @@
 <template>
      <Navbar :currentView="'info'" />
-     <InformationPanel></InformationPanel>
-    <InformationText></InformationText>
+     <div class="Main-Container">
+        <InformationPanel></InformationPanel>
+        <InformationText></InformationText>
+     </div>
+
 </template>
 
 <script setup>
@@ -10,3 +13,8 @@ import InformationPanel from '../components/InformationPanel.vue';
 import InformationText from '../components/InformationText.vue';
 
 </script>
+<style scoped>
+.Main-Container{
+    font-family: var(--fuente-principal);
+}
+</style>
