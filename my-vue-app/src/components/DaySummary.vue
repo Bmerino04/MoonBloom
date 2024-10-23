c372833d29f198c8db477aaa9989a90bd067aecf
vivivi <v.castro05@ufromail.cl>
2024-10-23 09:55:45 -0300

    feat DaySummary.vue json data

--- my-vue-app/src/components/DaySummary.vue
+++ my-vue-app/src/components/DaySummary.vue
@@ -2,58 +2,87 @@
     <div class="Day-Summary ">
         <div class="Phase-Info ">
             <div class="text">
-                        <p>Viernes, 25 Jul. </p>
-            <p>Fase Ovulatoria</p>
-            <h2>Periodo en <span>10 Días</span></h2>
-            <p>Prob. media de Embarazo</p>    
+                <p>{{ todayDate }} </p>
+                <p>{{ todayPhase }}</p>
+                <h2>Periodo en <span>{{daysLeft}} Días</span></h2>
+                <p>Prob. {{pregnancyProb}} de Embarazo</p>
             </div>
 
         </div>
     </div>
 </template>
+<script setup>
+import { ref, onMounted } from 'vue';
+import { getCalendarData } from '../Services/calendarServices';
 
+
+const todayDate = ref("");
+const todayPhase = ref("");
+const daysLeft = ref(0);
+const pregnancyProb =ref("");
+
+const loadCalendarData = async () => {
+  try {
+    const data = await getCalendarData();
+
+    todayDate.value = data.actualDate; 
+    todayPhase.value = data.currentPhase;
+    daysLeft.value = data.daysLeftPeriod;
+    pregnancyProb.value = data.pregnancyProbability;
+
+
+  } catch (error) {
+    console.error("Error al cargar los datos del calendario:", error);
+  }
+};
+onMounted(() => {
+  loadCalendarData();
+});
+
+
+</script>
 <style scoped>
-.Day-Summary{
-    width: 22.5rem;
-    height: 22.5rem;
+.Day-Summary {
+    width: 21.5rem;
+    height: 21.5rem;
     border: 0.6rem dashed #2c5d5bc1;
-    border-radius:100% ;
+    border-radius: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 }
-.Phase-Info{
-    width: 20rem;
-    height: 20rem;
+
+.Phase-Info {
+    width: 19.5rem;
+    height: 19.5rem;
     background-color: #F88FA3;
-    border: 0.375rem solid #2c5d5bc1; 
-    border-radius:100%;
+    border: 0.375rem solid #2c5d5bc1;
+    border-radius: 100%;
     display: flex;
     align-content: center;
-    text-align: center; 
+    text-align: center;
     justify-content: center;
     align-items: center;
 
- 
-}
-.text{
-    width: 10rem;
-    
-     
-  
 
 }
-.text p{
+
+.text {
+    width: 10rem;
+}
+
+.text p {
     font-size: 20px;
     color: #FFFFFF;
 }
 
-.text h2{
+.text h2 {
     color: #FF2D55;
     font-size: 2rem;
     font-weight: 600;
 }
-.text span{
+
+.text span {
     font-weight: 900;
 }
 </style>
