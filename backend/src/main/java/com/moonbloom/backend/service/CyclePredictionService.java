665e3bc47b1e1075d0b2ccdff1095af3ee890e0e
ValentinaC <v.cifuentes04@ufromail.cl>
2024-11-27 23:29:25 -0300

    feat(service): add initial logic for predictions

--- backend/src/main/java/com/moonbloom/backend/service/CyclePredictionService.java
+++ backend/src/main/java/com/moonbloom/backend/service/CyclePredictionService.java
@@ -1,12 +1,35 @@
 package com.moonbloom.backend.service;
 
+import com.moonbloom.backend.entities.Cycle;
 import com.moonbloom.backend.repository.CycleDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CyclePredictionService {
     @Autowired
     private CycleDataRepository cycleDataRepository;
+    public Map<String, Object> predictCycleDetails(Long userId) {
+        List<Cycle> cycles = cycleDataRepository.findByUserId(userId);
+
+        double avgCycleLength = calculateAverageCycleLength(cycles);
+        double avgMenstrualPhaseDuration = calculateAverageMenstrualPhaseDuration(cycles);
+        int avgDayOfOvulation = calculateAverageDayOfOvulation(avgCycleLength);
+
+        return null;
+    }
     
+    private double calculateAverageCycleLength(List<Cycle> cycles) {
+        return 0;
+    }
+    private double calculateAverageMenstrualPhaseDuration(List<Cycle> cycles) {
+        return 0;
+    }
+    private int calculateAverageDayOfOvulation(double avgCycleLength) {
+        return 0;
+    }
+
 }
